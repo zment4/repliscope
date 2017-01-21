7e33284a62b78444983a9e0747879c4403974a4b
Jani Kärkkäinen <jani.karkkainen@gmail.com>
2017-01-21 17:41:03 +0200

    Sinus Generator now works on both the main screen and game, and it quits after game end as well Added some media files

--- Assets/Scripts/SinusGenerator.cs
+++ Assets/Scripts/SinusGenerator.cs
@@ -3,9 +3,12 @@ using System;  // Needed for Math
 
 public class SinusGenerator : MonoBehaviour
 {
+    public SineWave SineWave;
     public SineWaveColorCompare SineWaveColorCompare;
     public float DistanceScale = 440;
     public bool PhaseShift = false;
+    public enum MonoModeEnum { Right, Left, Stereo };
+    public MonoModeEnum MonoMode;
 
     // un-optimized version
     public double frequency = 440;
@@ -17,22 +20,38 @@ public class SinusGenerator : MonoBehaviour
 
     void OnAudioFilterRead(float[] data, int channels)
     {
+        var waveLenT = SineWave ? (SineWave.WaveLength - SineWave.MinWavelength) / (SineWave.MaxWavelength - SineWave.MinWavelength) : 1f;
+
         // update increment in case frequency has changed
-        increment = frequency * 2 * Math.PI / sampling_frequency;
+        increment = (frequency + waveLenT * DistanceScale) * 2 * Math.PI / sampling_frequency;
         for (var i = 0; i < data.Length; i = i + channels)
         {
             phase = phase + increment;
+
+            var amplitudeT = SineWave ? (SineWave.Amplitude - SineWave.MinAmplitude) / (SineWave.MaxAmplitude - SineWave.MinAmplitude) : 1f;
             // this is where we copy audio data to make them “available” to Unity
-            data[i] = (float)(gain * Math.Sin(phase * (PhaseShift ? -1f : 1f)));
+            float audioData = (float)(gain * amplitudeT * Math.Sin(phase * (PhaseShift ? -1f : 1f)));
             // if we have stereo, we copy the mono data to each channel
-            if (channels == 2) data[i + 1] = data[i];
+            if (channels == 2)
+            {
+                if (MonoMode == MonoModeEnum.Left || MonoMode == MonoModeEnum.Stereo)
+                {
+                    data[i] = (GameManager.HasInstance && GameManager.Instance.HasGameEnded) ? 0f : audioData;
+                }
+
+                if (MonoMode == MonoModeEnum.Right || MonoMode == MonoModeEnum.Stereo)
+                {
+                    data[i + 1] = (GameManager.HasInstance && GameManager.Instance.HasGameEnded) ? 0f : audioData;
+                }
+            }
             if (phase > 2 * Math.PI) phase = 0;
         }
     }
 
     private void Update()
     {
-        frequency = 440 + SineWaveColorCompare.DistanceNormalized * DistanceScale;
+        if (SineWaveColorCompare)
+            frequency = 440 + SineWaveColorCompare.DistanceNormalized * DistanceScale;
     }
 }
 
